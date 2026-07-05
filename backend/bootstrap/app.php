f2cbabb216784a4b6c7a833c573fecfd47144c20
FOX <fox@FOX.ENVIRONMENT>
2026-07-05 13:29:44 +0100

    Configure dynamic API route prefixing for Vercel

--- backend/bootstrap/app.php
+++ backend/bootstrap/app.php
@@ -10,6 +10,7 @@ return Application::configure(basePath: dirname(__DIR__))
         web: __DIR__.'/../routes/web.php',
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
+        apiPrefix: (env('VIEW_COMPILED_PATH') === '/tmp') ? '' : 'api',
     )
     ->withMiddleware(function (Middleware $middleware): void {
         //
